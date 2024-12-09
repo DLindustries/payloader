db9af9b129863e048c5e7675669e7ac4c5fa0dfa
DLindustries <duncanl.studio@gmail.com>
2024-12-09 11:05:14 +0000

    no ip

--- api/sendMessage.js
+++ api/sendMessage.js
@@ -8,19 +8,14 @@ export default async function handler(req, res) {
     const { message } = req.body;
 
     try {
-        const ipResponse = await axios.get('https://api.ipify.org?format=json');
-        const ipAddress = ipResponse.data.ip;
-
         const now = new Date();
         const formattedDate = now.toLocaleDateString();
         const formattedTime = now.toLocaleTimeString();
-        const fullMessage = `**NEW SUGGESTION:** ${message}\n\n*Sent on ${formattedDate} at ${formattedTime}*\n\n*IP Address: ${ipAddress}*`;
+        const fullMessage = `**NEW SUGGESTION:** ${message}\n\n*Sent on ${formattedDate} at ${formattedTime}*`;
 
-        const webhookUrl = 'https://discord.com/api/webhooks/1311040088309760000/JY6VqkdSrSHrZfpFhvRKrIWZnZpi7Qgs45lPMgJvNh0kwqGGaCbPRNn--WuDCS4zW8cF';
+        const webhookUrl = 'https://discord.com/api/webhooks/1315628619225108562/D5kTXFozQeBABZhg9TrL_VKwRnX5RPUaYPbd1QfNmRQ4BaVTMY9qjLouQ7YWHux9q8F-';
 
-        const discordResponse = await axios.post(webhookUrl, { content: fullMessage });
-
-        console.log('Discord Response:', discordResponse.data);
+        await axios.post(webhookUrl, { content: fullMessage });
         res.status(200).json({ message: 'Message sent successfully' });
     } catch (error) {
         console.error('Error sending message:', error.message);
